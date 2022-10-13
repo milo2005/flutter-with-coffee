71365fe9e7d1365562733b7d2643308a67b582f1
Dario Chamorro <dario.chamorro@mercadolibre.com.co>
2022-10-13 16:40:47 -0500

    Internationalization example finished

--- docs/ui/intl.md
+++ docs/ui/intl.md
@@ -5,3 +5,11 @@
 
 ---
 - para textos custom si hay que agregar intl
+- Formatos de arb https://docs.google.com/document/d/10e0saTfAv32OZLRmONy866vnaw0I2jwL8zukykpgWBc/edit
+- Explicacion full https://localizely.com/flutter-arb/
+
+<key>CFBundleLocalizations</key>
+<array>
+    <string>en</string>
+    <string>hi</string>
+</array>
--- examples/internationalization/lib/HomePage.dart
+++ examples/internationalization/lib/HomePage.dart
@@ -6,12 +6,51 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final locations = AppLocalizations.of(context);
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.app_title),
+        title: Text(locations.appTitle),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              locations.initialGreetings("Dario"),
+              style: textTheme.headline6,
+            ),
+            Text(
+              locations.currentDay(DateTime.now()),
+              style: textTheme.bodyText2,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              locations.description,
+              style: textTheme.bodyText2,
+            ),
+            ListTile(
+              title: Text(locations.productCategories("phone")),
+              subtitle: Text(locations.productsCount(0)),
+            ),
+            ListTile(
+              title: Text(locations.productCategories("watch")),
+              subtitle: Text(locations.productsCount(1)),
+            ),
+            ListTile(
+              title: Text(locations.productCategories("tablet")),
+              subtitle: Text(locations.productsCount(10)),
+            ),
+            const Spacer(),
+            Text(
+              locations.money(10000),
+              style: textTheme.headline6,
+              textAlign: TextAlign.end,
+            ),
+          ],
+        ),
       ),
     );
   }
